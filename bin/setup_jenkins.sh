c27e0e2162a6d41dc7ad89f9d7b61331c82898a7
Yelken Gonzales <yelkezin@gmail.com>
2019-08-19 11:26:52 -0300

    Correções

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -13,33 +13,50 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
+oc new-app jenkins-persistent \
+    --param ENABLE_OAUTH=true \
+    --param MEMORY_LIMIT=2Gi \
+    --param VOLUME_CAPACITY=4Gi \
+    --param DISABLE_ADMINISTRATIVE_MONITORS=true \
+    -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
-oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
-      USER root \n
-      RUN yum -y install skopeo && yum clean all\n
-      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+oc new-build  \
+      -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+                USER root \n
+                RUN yum -y install skopeo && yum clean all\n
+                USER 1001' \
+      --name=jenkins-agent-appdev \
+      -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
-echo "apiVersion: v1
-items:
-- kind: "BuildConfig"
-  apiVersion: "v1"
-  metadata:
-    name: "tasks-pipeline"
-  spec:
-    source:
-      type: "Git"
-      contextDir: "openshift-tasks"
-      git:
-        uri: "${REPO}"
-    strategy:
-      type: "JenkinsPipeline"
-      jenkinsPipelineStrategy:
-        jenkinsfilePath: Jenkinsfile
-kind: List
-metadata: []" | oc create -f - -n ${GUID}-jenkins
+oc -n $GUID-jenkins new-build ${REPO} \
+   --strategy pipeline \
+   --env GUID=${GUID} \
+   --env CLUSTER=${CLUSTER} \
+   --env NEXUS_RELEASE_URL='http://nexus3.gpte-hw-cicd.svc:8081/repository/releases' \
+   --env REPO=${REPO} \
+   --context-dir openshift-tasks \
+   --name=tasks-pipeline
+
+# echo "apiVersion: v1
+# items:
+# - kind: "BuildConfig"
+#   apiVersion: "v1"
+#   metadata:
+#     name: "tasks-pipeline"
+#   spec:
+#     source:
+#       type: "Git"
+#       contextDir: "openshift-tasks"
+#       git:
+#         uri: "${REPO}"
+#     strategy:
+#       type: "JenkinsPipeline"
+#       jenkinsPipelineStrategy:
+#         jenkinsfilePath: Jenkinsfile
+# kind: List
+# metadata: []" | oc create -f - -n ${GUID}-jenkins
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do

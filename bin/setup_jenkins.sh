7406fe7fdf1f0a6884429a638fce4bf65ebe15eb
Yelken Gonzales <yelkezin@gmail.com>
2019-09-20 09:22:48 -0300

    Correções

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Setup Jenkins Project
-# set -x
 if [ "$#" -ne 3 ]; then
     echo "Usage:"
     echo "  $0 GUID REPO CLUSTER"
@@ -14,47 +13,37 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-# TBD
-# Mine
-# oc new-project ${GUID}-jenkins
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n "${GUID}-jenkins"
-# oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true  -n ${GUID}-jenkins
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m  -n ${GUID}-jenkins
 
+oc expose svc/jenkins  -n ${GUID}-jenkins
 # Create custom agent container image with skopeo
-# TBD
-# Mine
 oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
       USER root\nRUN yum -y install skopeo && yum clean all\n
-      USER 1001' --name=jenkins-agent-appdev -n "${GUID}-jenkins"
+      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
-# TBD
-# Mine
 echo "apiVersion: v1
 items:
-- kind: "BuildConfig"
-  apiVersion: "v1"
+- kind: 'BuildConfig'
+  apiVersion: 'v1'
   metadata:
-    name: "tasks-pipeline"
+    name: 'tasks-pipeline'
   spec:
     source:
-      type: "Git"
+      contextDir: openshift-tasks
+      type: 'git'
       git:
-        uri: "${REPO}"
-      contextDir: "openshift-tasks"
+        uri: '${REPO}'
     strategy:
-      type: "JenkinsPipeline"
+      type: 'JenkinsPipeline'
       jenkinsPipelineStrategy:
-        jenkinsfilePath: Jenkinsfile
         env:
-        - name: "GUID"
-          value: "${GUID}"
-        - name: "REPO"
-          value: "${REPO}"
-        - name: "CLUSTER"
-          value: "${CLUSTER}"
+        - name: GUID
+          value: ${GUID}
+        jenkinsfilePath: Jenkinsfile
 kind: List
-metadata: []" | oc create -f - -n "${GUID}-jenkins"
+metadata: []" | oc apply -f - -n ${GUID}-jenkins
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
--- bin/setup_prod.sh
+++ bin/setup_prod.sh
@@ -20,8 +20,8 @@ oc expose dc tasks-blue --port 8080 -n ${GUID}-tasks-prod
 oc create configmap tasks-blue-config --from-literal="application-users.properties=Placeholder" --from-literal="application-roles.properties=Placeholder" -n ${GUID}-tasks-prod
 oc set volume dc/tasks-blue --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-blue-config -n ${GUID}-tasks-prod
 oc set volume dc/tasks-blue --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-blue-config -n ${GUID}-tasks-prod
-oc set probe dc/tasks-blue --readiness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
-oc set probe dc/tasks-blue --liveness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-blue --readiness --get-url=http://:8080/ --initial-delay-seconds=40 --timeout-seconds=1 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-blue --liveness --get-url=http://:8080/ --initial-delay-seconds=40 --timeout-seconds=1 -n ${GUID}-tasks-prod
 # Setting 'wrong' VERSION. This will need to be updated in the pipeline
 oc set env dc/tasks-blue VERSION='0.0 (tasks-blue)' -n ${GUID}-tasks-prod
 
@@ -33,10 +33,10 @@ oc expose dc tasks-green --port 8080 -n ${GUID}-tasks-prod
 oc create configmap tasks-green-config --from-literal="application-users.properties=Placeholder" --from-literal="application-roles.properties=Placeholder" -n ${GUID}-tasks-prod
 oc set volume dc/tasks-green --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-green-config -n ${GUID}-tasks-prod
 oc set volume dc/tasks-green --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-green-config -n ${GUID}-tasks-prod
-oc set probe dc/tasks-green --readiness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
-oc set probe dc/tasks-green --liveness --get-url=http://:8080/ --initial-delay-seconds=60 --timeout-seconds=1 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-green --readiness --get-url=http://:8080/ --initial-delay-seconds=40 --timeout-seconds=1 -n ${GUID}-tasks-prod
+oc set probe dc/tasks-green --liveness --get-url=http://:8080/ --initial-delay-seconds=40 --timeout-seconds=1 -n ${GUID}-tasks-prod
 # Setting 'wrong' VERSION. This will need to be updated in the pipeline
-oc set env dc/tasks-green VERSION='0.0 (tasks-green)' -n ${GUID}-tasks-prod
+oc set env dc/tasks-green VERSION='0.0 (tsks-green)' -n ${GUID}-tasks-prod
 
 # Expose Blue service as route to make green application active
 oc expose svc/tasks-green --name tasks -n ${GUID}-tasks-prod
